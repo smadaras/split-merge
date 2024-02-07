0680fea7f0a38f52993161d2a85f2a6f6b759206
Sandor Madaras <sandor@madaras.website>
2024-02-07 11:13:14 +0100

    No default file names

--- decode-merge.py
+++ decode-merge.py
@@ -5,7 +5,7 @@ def merge_files(output_file):
     with open(output_file, 'wb') as out_file:
         index = 0
         while True:
-            file_name = f'chunk_{index}.txt'
+            file_name = f'{output_file}_{index}.txt'
             print(file_name)
             try:
                 with open(file_name, 'r') as in_file:
@@ -18,9 +18,10 @@ def merge_files(output_file):
             index += 1
     
 if __name__ == "__main__":
-    output_file = 'merged_binary_file.bin'
-
-    if len(sys.argv) > 1:
+    if len(sys.argv) < 2:
+        print(f"Use: python decode-merge.py outputfile")
+        exit(-1)
+    else:
         output_file = sys.argv[1]
          
 # Merge the smaller pieces back into the original file
--- split-encode.py
+++ split-encode.py
@@ -9,7 +9,7 @@ def split_file(input_file, chunk_size):
             if not chunk:
                 break
             text_data = base64.b64encode(chunk).decode('utf-8')
-            chunk_file = f'chunk_{index}.txt'
+            chunk_file = f'{input_file}_{index}.txt'
             with open(chunk_file, 'w') as f:
                 f.write(text_data)
             index += 1
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     chunk_size = 1024
 
     if len(sys.argv) < 2:
-        print(f"Use: python split-encode input.bin [chunk size]")
+        print(f"Use: python split-encode.py input.bin [chunk size]")
         exit(-1)
     input_file = sys.argv[1]
     if len(sys.argv) > 2:
